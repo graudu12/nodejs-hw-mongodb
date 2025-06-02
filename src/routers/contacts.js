779f771a51192d7bc35b516de46f086ee9204443
Dima <graudu_12@yahoo.co.uk>
2025-06-02 22:30:05 +0100

    commnts

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -16,7 +16,7 @@ import { isValidId } from '../middlewares/isValidId.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
-
+//comments
 router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
